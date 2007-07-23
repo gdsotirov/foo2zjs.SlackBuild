6f9b3fcee65e764b8181835a3002db5415e09a56
Georgi D. Sotirov <gdsotirov@dir.bg>
2007-07-23 20:23:58 +0000

    Change information messages, add hplj1018

--- doinst.sh
+++ doinst.sh
@@ -1,5 +1,7 @@
-echo -n "Installing usermaps..."
+echo -n "Installing user maps..."
 /etc/hotplug/usb/hplj1000 install-usermap
 /etc/hotplug/usb/hplj1005 install-usermap
+/etc/hotplug/usb/hplj1018 install-usermap
 /etc/hotplug/usb/hplj1020 install-usermap
-echo "Done."
+echo "Done"
+
